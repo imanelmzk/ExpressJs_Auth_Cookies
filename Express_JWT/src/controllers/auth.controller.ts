8e24c49b9bbac08cfb3435b98f02301c38f4d891
imane.lmzk <imane.lmzk@gmail.com>
2026-06-24 23:16:34 +0100

    update ORM

--- Express_JWT/src/controllers/auth.controller.ts
+++ Express_JWT/src/controllers/auth.controller.ts
@@ -1,27 +1,41 @@
 import {Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
-
-export const register = (req : Request, res : Response) =>{
-    res.send("Register endpoint, or User registered successfully");
-};
-
-export const login = (req:Request, res: Response) =>{
+import { createUser, findUserByEmail } from '../services/auth.service';
+export const register = async (req : Request, res : Response) =>{
     const {email, password} = req.body;
 
-    const fakeUser = {
+    try {
+        const user = await createUser(email, password);
+        res.status(201).json({
+            message : "User registered successfully",
+            user
+        });
+    } catch (error) {
+        res.status(400).json({
+            message : "Error registering user"
+        });
+    }
+};
+
+export const login = async(req:Request, res: Response) =>{
+    const {email, password} = req.body;
+
+    const user = await findUserByEmail(email);
+    /*const fakeUser = {
         email : "test@test.com",
         password : "123456"
-    };
+    };*/
 
-    if(email !== fakeUser.email || password !== fakeUser.password) {
+    if(!user || user.password !== password){ {
         return res.status(401).json({
             message : "Invalid credentials"
         });
     }
+}
 
     // Generate JWT token
     const token = jwt.sign(
-        {email : fakeUser.email}, // payload
+        {email : user.email}, // payload
         process.env.JWT_SECRET as string, // secret key
         {expiresIn : "1h"} // options : durrée de validité du token
     )
@@ -31,17 +45,19 @@ export const login = (req:Request, res: Response) =>{
         email,
         token
     })*/
-   return res.cookie("token", token, {
-        httpOnly : true, // set to true in production with HTTPS
-        secure : process.env.NODE_ENV === "production",// Seulement HTTPS
-        sameSite : "strict", // Empeche attaques CSRF (Protection CSRF)
-        maxAge : 3600000 // 1 hour == Durée de vie  
+   
+   return res
+        .cookie("token", token, {
+            httpOnly : true, // set to true in production with HTTPS
+            secure : process.env.NODE_ENV === "production",// Seulement HTTPS
+            sameSite : "strict", // Empeche attaques CSRF (Protection CSRF)
+            maxAge : 3600000 // 1 hour == Durée de vie  
         
-   })
-    .json({
-        message : "login successful",
-        email,
-        token
+        })
+        .json({
+            message : "login successful",
+            email,
+            token
     });
     
 }
---
+++ Express_JWT/src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import {PrismaClient} from "../generated/prisma";
+
+const prisma = new PrismaClient();
+export const createUser = async (email: string, password: string)=>{
+    return await prisma.user.create({
+        data: {
+            email,
+            password
+        }
+    });
+}
+
+export const findUserByEmail = async( email : string) =>{
+    return await prisma.user.findUnique({
+        where : {
+            email
+        }
+    });
+}
